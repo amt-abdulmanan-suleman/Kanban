573164523ff0f181092bfd5064d0050340513937
Abdul Suleman <abdul.suleman@amalitech.com>
2024-03-07 16:14:37 +0000

    feat: full todo list appp in angular

---
+++ src/app/pages/main-view/main-view.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+
+import { CommonModule } from '@angular/common';
+import { dummyData } from '../../../data';
+
+interface  DummyData{
+  todos: string[];
+  done: string[];
+  ideas: string[];
+  research: string[];
+}
+@Component({
+  selector: 'app-main-view',
+  standalone: true,
+  imports: [CdkDropListGroup, CdkDropList, CdkDrag, CommonModule],
+  templateUrl: './main-view.component.html',
+  styleUrl: './main-view.component.scss'
+})
+export class MainViewComponent {
+  tasks = dummyData
+
+
+  drop(event: any) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
---
+++ src/data.ts
@@ -0,0 +1,43 @@
+export const dummyData = {
+    columns: [
+      {
+        title: "Todos",
+        tasks: [
+          "Prepare breakfast",
+          "Go for a morning walk",
+          "Attend meeting at 10:00 AM",
+          "Complete project tasks",
+        ]
+      },
+      {
+        title: "Done",
+        tasks: [
+          "Send report to manager",
+          "Review code changes",
+          "Organize desk",
+          "File expense reports",
+          "Update project status",
+        ]
+      },
+      {
+        title: "Ideas",
+        tasks: [
+          "Brainstorm new project ideas",
+          "Research market trends",
+          "Explore potential collaborations",
+          "Create mind maps",
+
+        ]
+      },
+      {
+        title: "Research",
+        tasks: [
+          "Investigate best practices",
+          "Study market trends",
+          "Analyze competitors",
+          "Conduct user surveys",
+        ]
+      }
+    ]
+  };
+  
